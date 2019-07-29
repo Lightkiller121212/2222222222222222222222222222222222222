9f90f7c091f71f79c4f807b9e7b8781aa83eb627
Lightkiller121212 <52613551+Lightkiller121212@users.noreply.github.com>
2019-07-29 12:07:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+
 
 //page help
 
